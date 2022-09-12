446640a848ce0633461ffc88f4a4eea09a725968
Heni Mezrani <orangeodcmacbook2@MacBook-Pro-de-Orangeodc-3.local>
2022-09-12 14:07:23 +0100

    testing auto deploys

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use("/", express.static("public"))
-
 app.listen(PORT || 3000, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
